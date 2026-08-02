b6c9743acc50c4de371e0dcb67596b3c4e208595
AuthAnkit <authankit@gmail.com>
2026-08-02 18:02:51 +0530

    Initial commit

---
+++ src/test/java/com/AutoEmailMate/eg/EgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoEmailMate.eg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
